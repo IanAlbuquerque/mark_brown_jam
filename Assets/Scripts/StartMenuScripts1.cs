b7dfea08b7365a55e15153a5db14ceeee128160b
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2018-09-02 16:44:21 -0300

    bug fixes

---
+++ Assets/Scripts/StartMenuScripts1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScripts1 : MonoBehaviour {
+
+	public Button newGameButton;
+	public Button quitButton;
+
+
+	// Use this for initialization
+	void Start () {
+		this.newGameButton.onClick.AddListener(this.onClickNewGame);
+		this.quitButton.onClick.AddListener(this.onClickQuit);
+	}
+
+	public void onClickNewGame() {
+		SceneManager.LoadScene("MenuScene");
+	}
+
+	public void onClickQuit() {
+		Application.Quit();
+	}
+
+}
